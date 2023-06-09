e9c071eaabab429d3a5cdcac311637be3cfc1cd8
Евгений Мишарев <misharevea@gmail.com>
2023-06-09 10:53:41 +0300

    Обновление отметок Weandrevit

--- GloryHoleRefreshElevations/GloryHoleRefreshElevationsCommand.cs
+++ GloryHoleRefreshElevations/GloryHoleRefreshElevationsCommand.cs
@@ -16,7 +16,7 @@ namespace GloryHoleRefreshElevations
             Guid heightOfBaseLevelGuid = new Guid("9f5f7e49-616e-436f-9acc-5305f34b6933");
             Guid levelOffsetGuid = new Guid("515dc061-93ce-40e4-859a-e29224d80a10");
 
-            List<FamilyInstance> intersectionPointRectangularWallFamilyInstanceList = new FilteredElementCollector(doc)
+            List<FamilyInstance> intersectionPointFamilyInstanceList = new FilteredElementCollector(doc)
                 .OfCategory(BuiltInCategory.OST_GenericModel)
                 .OfClass(typeof(FamilyInstance))
                 .WhereElementIsNotElementType()
@@ -33,17 +33,29 @@ namespace GloryHoleRefreshElevations
                 || ip.Symbol.Family.Name == "Гильза_Плита")
                 .ToList();
 
+            List<FamilyInstance> intersectionPointWeandrevitList = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Windows)
+                .OfClass(typeof(FamilyInstance))
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .Where(ip => ip.Symbol.Family.Name == "231_Отверстие прямоугольное (Окно_Стена)"
+                || ip.Symbol.Family.Name == "231_Отверстие круглое с гильзой в стене (Окно_Стена)")
+                .ToList();
+
             using (Transaction t = new Transaction(doc))
             {
                 t.Start("Обновление отметок");
-                foreach (FamilyInstance intersectionPoint in intersectionPointRectangularWallFamilyInstanceList)
+                foreach (FamilyInstance intersectionPoint in intersectionPointFamilyInstanceList)
                 {
                     intersectionPoint.get_Parameter(heightOfBaseLevelGuid).Set((doc.GetElement(intersectionPoint.LevelId) as Level).Elevation);
                     if(intersectionPoint.Symbol.FamilyName == "Пересечение_Плита_Прямоугольное"
                         || intersectionPoint.Symbol.FamilyName == "Пересечение_Плита_Круглое")
 
                     {
-                        intersectionPoint.get_Parameter(levelOffsetGuid).Set(intersectionPoint.get_Parameter(BuiltInParameter.INSTANCE_FREE_HOST_OFFSET_PARAM).AsDouble() - 50 / 304.8);
+                        if(intersectionPoint.get_Parameter(levelOffsetGuid) != null)
+                        {
+                            intersectionPoint.get_Parameter(levelOffsetGuid).Set(intersectionPoint.get_Parameter(BuiltInParameter.INSTANCE_FREE_HOST_OFFSET_PARAM).AsDouble() - 50 / 304.8);
+                        }
                     }
                     else if(intersectionPoint.Symbol.FamilyName == "Отверстие_Плита_Прямоугольное"
                         || intersectionPoint.Symbol.FamilyName == "Отверстие_Плита_Круглое"
@@ -51,14 +63,31 @@ namespace GloryHoleRefreshElevations
                     {
                         if (intersectionPoint.Host != null)
                         {
-                            intersectionPoint.get_Parameter(levelOffsetGuid).Set(doc.GetElement(intersectionPoint.Host.Id).get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).AsDouble());
+                            if(intersectionPoint.get_Parameter(levelOffsetGuid) != null)
+                            {
+                                intersectionPoint.get_Parameter(levelOffsetGuid).Set(doc.GetElement(intersectionPoint.Host.Id).get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).AsDouble());
+                            }
                         }
                         else
                         {
-                            intersectionPoint.get_Parameter(levelOffsetGuid).Set(0);
+                            if(intersectionPoint.get_Parameter(levelOffsetGuid) != null)
+                            {
+                                intersectionPoint.get_Parameter(levelOffsetGuid).Set(0);
+                            }
                         }
                     }
                     else
+                    {
+                        if(intersectionPoint.get_Parameter(levelOffsetGuid) != null)
+                        {
+                            intersectionPoint.get_Parameter(levelOffsetGuid).Set(intersectionPoint.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).AsDouble());
+                        }
+                    }
+                }
+
+                foreach (FamilyInstance intersectionPoint in intersectionPointWeandrevitList)
+                {
+                    if(intersectionPoint.get_Parameter(levelOffsetGuid) != null)
                     {
                         intersectionPoint.get_Parameter(levelOffsetGuid).Set(intersectionPoint.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).AsDouble());
                     }
